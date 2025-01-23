3a4e10f2c56ce3cee3faf4e2f03cdfc5cf811633
Chocobo <moras.thomas@gmail.com>
2025-01-23 17:03:11 +0100

    testing deploy & work in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div> Home</div>;
+  return (
+    <div className=" flex flex-col justify-center items-center">
+      <h1>Work In Progress</h1>
+      <div className="justify-center items-center">
+        <Image
+          src="/images/work_in_progress.jpg"
+          width={500}
+          height={500}
+          alt="Work In Progress"
+        />
+      </div>
+    </div>
+  );
 }
